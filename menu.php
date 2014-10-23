678dc234a4cc3d5412c8225e9e076322e579e2a8
Jon <jonneyblueeyesuk@hotmail.com>
2014-10-23 17:21:31 +0100

    hello

---
+++ menu.php
@@ -0,0 +1,7 @@
+<script type="text/javascript" src="menu/js/stmenu.js"></script>
+
+
+<div style='position:absolute; left:182px; top:90px;'>
+<script type="text/javascript" src="menu/hipMenu.js"></script>
+
+</div>
